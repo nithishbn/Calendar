e89e36c72e14f36573e06a8a66dab3dc8c00bc5f
Nithish Narasimman <nithishthegreat7@gmail.com>
2017-12-29 20:01:31 -0800

    minor fixes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from kivy.animation import Animation
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.metrics import dp
-from kivy.properties import StringProperty, Clock, ListProperty, NumericProperty
+from kivy.properties import StringProperty, Clock, ListProperty, NumericProperty, partial
 from kivy.uix.screenmanager import Screen
 from kivy.uix.scrollview import ScrollView
 from kivymd.button import MDIconButton
@@ -228,13 +228,14 @@ class DetailsScreen(Screen):
     date = StringProperty('')
 
     def on_pre_enter(self, *args):
-        self.search(self.date)
+        Clock.schedule_once(self.search)
         # if success == -1:
         #     self.count -= 1
         #     print("leaving detailsscreen", self.count)
         #     self.manager.current = "DateScreen"
 
-    def search(self, date):
+    def search(self, *args):
+        date = self.date
         conn = sqlite3.connect('data.sqlite')
         cur = conn.cursor()
         cur.execute(
@@ -284,86 +285,99 @@ WHERE "﻿Date" = ?
         else:
             date1 = datetime.strptime(date, "%d-%b-%y")
             self.date = date1.strftime("%B %d") + " Events"
-            for obj in self.ids.labels.ids:
-                self.ids.labels.remove_widget(obj)
-            samva = MDLabel(text=data[1] + " nama samvatsarae", pos_hint={'x': 0.05}, font_style="Subhead",
+            
+            samva = MDLabel(id="samva", text=data[1] + " nama samvatsarae", pos_hint={'x': 0.05}, font_style="Subhead",
                             halign="left", theme_text_color="Primary")
             self.ids.labels.add_widget(samva)
-            ayanam = MDLabel(text=data[2], pos_hint={'x': 0.05}, font_style="Subhead", halign="left",
+            print(self.ids.labels.ids)
+            ayanam = MDLabel(id="ayanam", text=data[2], pos_hint={'x': 0.05}, font_style="Subhead", halign="left",
                              theme_text_color="Primary")
             self.ids.labels.add_widget(ayanam)
-            rithu = MDLabel(text=data[3] + " rithu", pos_hint={'x': 0.05}, font_style="Subhead", halign="left",
+            rithu = MDLabel(id="rithu", text=data[3] + " rithu", pos_hint={'x': 0.05}, font_style="Subhead",
+                            halign="left",
                             theme_text_color="Primary")
             self.ids.labels.add_widget(rithu)
-            maasa = MDLabel(text="Tamil Maasa: " + data[4] + " maasae", pos_hint={'x': 0.05}, font_style="Subhead",
+            maasa = MDLabel(id="maasa", text="Tamil Maasa: " + data[4] + " maasae", pos_hint={'x': 0.05},
+                            font_style="Subhead",
                             halign="left", theme_text_color="Primary")
             self.ids.labels.add_widget(maasa)
-            tmaasa = MDLabel(text="Telugu Maasa: " + data[5] + " maasae", pos_hint={'x': 0.05}, font_style="Subhead",
+            tmaasa = MDLabel(id="tmaasa", text="Telugu Maasa: " + data[5] + " maasae", pos_hint={'x': 0.05},
+                             font_style="Subhead",
                              halign="left", theme_text_color="Primary")
             self.ids.labels.add_widget(tmaasa)
-            paksha = MDLabel(text=data[6] + " paaksha", pos_hint={'x': 0.05}, font_style="Subhead", halign="left",
+            paksha = MDLabel(id="paksha", text=data[6] + " paaksha", pos_hint={'x': 0.05}, font_style="Subhead",
+                             halign="left",
                              theme_text_color="Primary")
             self.ids.labels.add_widget(paksha)
             thithi1Time = data[8]
-            thithi1 = MDLabel(text=data[7] + " thithu starts at " + thithi1Time.strip(), pos_hint={'x': 0.05},
+            thithi1 = MDLabel(id="thithi1", text=data[7] + " thithu starts at " + thithi1Time.strip(),
+                              pos_hint={'x': 0.05},
                               font_style="Subhead", halign="left", theme_text_color="Primary")
             self.ids.labels.add_widget(thithi1)
 
             if data[9] is not None:
                 thithi2Time = data[10]
                 if thithi2Time == "(whole day)":
-                    thithi2 = MDLabel(text=data[9] + " thithu lasts the entire day", pos_hint={'x': 0.05},
+                    thithi2 = MDLabel(id="thithi2", text=data[9] + " thithu lasts the entire day", pos_hint={'x': 0.05},
                                       font_style="Subhead", halign="left", theme_text_color="Primary")
                     self.ids.label.add_widget(thithi2)
                 else:
-                    thithi2 = MDLabel(text=data[9] + " thithu starts at " + thithi2Time.strip(), pos_hint={'x': 0.05},
+                    thithi2 = MDLabel(id="thithi2", text=data[9] + " thithu starts at " + thithi2Time.strip(),
+                                      pos_hint={'x': 0.05},
                                       font_style="Subhead", halign="left", theme_text_color="Primary")
                     self.ids.label.add_widget(thithi2)
-            vara = MDLabel(text=data[11], pos_hint={'x': 0.05}, font_style="Subhead",
+            vara = MDLabel(id="vara", text=data[11], pos_hint={'x': 0.05}, font_style="Subhead",
                            halign="left", theme_text_color="Primary")
             self.ids.labels.add_widget(vara)
             nakshatraTime = data[13]
-            nakshatra = MDLabel(text=data[12] + " nakshatra starts at " + nakshatraTime.strip(), pos_hint={'x': 0.05},
+            nakshatra = MDLabel(id="nakshatra", text=data[12] + " nakshatra starts at " + nakshatraTime.strip(),
+                                pos_hint={'x': 0.05},
                                 font_style="Subhead",
                                 halign="left", theme_text_color="Primary")
             self.ids.labels.add_widget(nakshatra)
             if data[20] is not "" and data[20] != "NULL":
-                self.ids.labels.add_widget(MDLabel(text="Rahu Kalam: " + data[20], theme_text_color='Primary',
-                                                   font_style='Subhead', pos_hint={'x': 0.05}))
+                self.ids.labels.add_widget(
+                    MDLabel(id="rahu", text="Rahu Kalam: " + data[20], theme_text_color='Primary',
+                            font_style='Subhead', pos_hint={'x': 0.05}))
             if data[21] is not "" and data[21] != "NULL":
-                self.ids.labels.add_widget(MDLabel(text="Varjyam: " + data[21], theme_text_color='Primary',
-                                                   font_style='Subhead', pos_hint={'x': 0.05}))
+                self.ids.labels.add_widget(
+                    MDLabel(id="varjyam", text="Varjyam: " + data[21], theme_text_color='Primary',
+                            font_style='Subhead', pos_hint={'x': 0.05}))
             if data[22] is not "" and data[22] != "NULL":
-                self.ids.labels.add_widget(MDLabel(text="Durmhurtam: " + data[22], theme_text_color='Primary',
-                                                   font_style='Subhead', pos_hint={'x': 0.05}))
+                self.ids.labels.add_widget(
+                    MDLabel(id="durmu", text="Durmhurtam: " + data[22], theme_text_color='Primary',
+                            font_style='Subhead', pos_hint={'x': 0.05}))
             if data[14] is not None:
                 nakshatra2Time = data[15]
                 if self.nakshatra2Time == "Whole day":
-                    nakshatra2 = MDLabel(text=data[14] + " nakshatra lasts the entire day", pos_hint={'x': 0.05},
+                    nakshatra2 = MDLabel(id="nakshatra2", text=data[14] + " nakshatra lasts the entire day",
+                                         pos_hint={'x': 0.05},
                                          font_style="Subhead", halign="left", theme_text_color="Primary")
                     self.ids.label.add_widget(nakshatra2)
                 else:
-                    nakshatra2 = MDLabel(text=data[14] + " nakshatra starts at " + self.nakshatra2Time.strip(),
+                    nakshatra2 = MDLabel(id="nakshatra2",
+                                         text=data[14] + " nakshatra starts at " + self.nakshatra2Time.strip(),
                                          pos_hint={'x': 0.05},
                                          font_style="Subhead", halign="left", theme_text_color="Primary")
                     self.ids.label.add_widget(nakshatra2)
             if data[16] is "" and data[17] is "" and data[18] is "" and data[19] is "":
-                self.ids.notesList.add_widget(MDLabel(text="No special events today!", theme_text_color='Primary',
-                                                      font_style='Subhead', pos_hint={'x': 0.05, 'top': 1}))
+                self.ids.notesList.add_widget(
+                    MDLabel(id="note0", text="No special events today!", theme_text_color='Primary',
+                            font_style='Subhead', pos_hint={'x': 0.05, 'top': 1}))
             else:
                 if data[16] is not "":
-                    self.ids.notesList.add_widget(MDLabel(text=data[16], theme_text_color='Primary',
+                    self.ids.notesList.add_widget(MDLabel(id="note1", text=data[16], theme_text_color='Primary',
                                                           font_style='Subhead', pos_hint={'x': 0.05, 'top': 1}))
                 if data[17] is not "":
-                    self.ids.notesList.add_widget(MDLabel(text=data[17], theme_text_color='Primary',
+                    self.ids.notesList.add_widget(MDLabel(id="note2", text=data[17], theme_text_color='Primary',
                                                           font_style='Subhead', pos_hint={'x': 0.05, 'top': 1}))
                 if data[18] is not "":
                     print("rogerroger")
-                    self.ids.notesList.add_widget(MDLabel(text=data[18], theme_text_color='Custom',
+                    self.ids.notesList.add_widget(MDLabel(id="note3", text=data[18], theme_text_color='Custom',
                                                           font_style='Subhead', pos_hint={'x': 0.05, 'top': 1}))
                 if data[19] is not "":
                     print("roger")
-                    self.ids.notesList.add_widget(MDLabel(text=data[19], theme_text_color='Primary',
+                    self.ids.notesList.add_widget(MDLabel(id="note4", text=data[19], theme_text_color='Primary',
                                                           font_style='Subhead', pos_hint={'x': 0.05}))
 
 
